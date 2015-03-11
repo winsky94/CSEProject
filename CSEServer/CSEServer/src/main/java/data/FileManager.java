4a9ad16298808083ec2d04e5aa78aa7fc8f63f68
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-03-11 06:53:50 +0000

    将迭代一数据上传

--- CSEServer/CSEServer/src/main/java/data/FileManager.java
+++ CSEServer/CSEServer/src/main/java/data/FileManager.java
@@ -11,72 +11,74 @@ import java.util.ArrayList;
 import po.PlayerPO;
 
 public class FileManager {
-	
-	 public PlayerPO readFromPlayerFile(String fileName){
-		int count=0;
+
+	public PlayerPO readFromPlayerFile(String fileName) {
+		int count = 0;
 		PlayerPO player;
 		String mid;
 		String mname;
 		int mnumber;
 		String mposition;
 		String mheight;
-		String mweight;
+		int mweight;
 		String mbirth;
 		int mage;
 		int mexp;
 		String mschool;
-		String[] content=new String[9];
-		try{
-			
-			//File file=new File("13-14_01-01_CHA-LAC");
-			File file=new File(fileName);
-	        if(!file.exists()){
-	        	
-	        	file.createNewFile();	            
-	        }
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
-			int i=0;
-	        String temp = null;
-	        temp = br.readLine();
-	        while(temp!=null){
-//	        	System.out.println(temp);
-	        	if(temp.contains("│")){
-	        	String[] it=temp.split("│");
-	        	String[] nit=it[1].split("║");
-	        	content[i++]=nit[0].trim();
-	        	}
-	            temp = br.readLine();
-	        }
-	        
-	        br.close();
-		}
-		catch (IOException e){
+		String[] content = new String[9];
+		try {
+
+			// File file=new File("13-14_01-01_CHA-LAC");
+			File file = new File(fileName);
+			if (!file.exists()) {
+
+				file.createNewFile();
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new FileInputStream(file), "UTF-8"));
+			int i = 0;
+			String temp = null;
+			temp = br.readLine();
+			while (temp != null) {
+				// System.out.println(temp);
+				if (temp.contains("│")) {
+					String[] it = temp.split("│");
+					String[] nit = it[1].split("║");
+					content[i++] = nit[0].trim();
+				}
+				temp = br.readLine();
+			}
+
+			br.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		NumberFormat nf = NumberFormat.getInstance();
-		nf.setMinimumIntegerDigits(5); 
+		nf.setMinimumIntegerDigits(5);
 		nf.setGroupingUsed(false);
-		mid=nf.format(count);
-		mname=content[0];
-		mnumber=Integer.parseInt(content[1]);
-		mposition=content[2];
-		mheight=content[3];
-		mweight=content[4];
-		mbirth=content[5];
-		mage=Integer.parseInt(content[6]);
-		mexp=Integer.parseInt(content[7]);
-		mschool=content[8];
-		player=new PlayerPO(mid,mname,mnumber,mposition,mheight,mweight,mbirth,mage,mexp,mschool);
+		mid = nf.format(count);
+		mname = content[0];
+		mnumber = Integer.parseInt(content[1]);
+		mposition = content[2];
+		mheight = content[3];
+		mweight = Integer.parseInt(content[4]);
+		mbirth = content[5];
+		mage = Integer.parseInt(content[6]);
+		mexp = Integer.parseInt(content[7]);
+		mschool = content[8];
+		player = new PlayerPO(mid, mname, mnumber, mposition, mheight, mweight,
+				mbirth, mage, mexp, mschool);
 		return player;
 	}
-	 
-	 public ArrayList<PlayerPO> readAllPlayers(){
-		 ArrayList<PlayerPO> players=new ArrayList<PlayerPO>();
-		 try {
-			FileList fl=new FileList("D:/LUCY/Documents/软件工程与计算III/data/迭代一数据/players/info");
-			ArrayList<String> names=fl.getList();
-			for(String name:names){
+
+	public ArrayList<PlayerPO> readAllPlayers() {
+		ArrayList<PlayerPO> players = new ArrayList<PlayerPO>();
+		try {
+			FileList fl = new FileList(
+					"D:/LUCY/Documents/软件工程与计算III/data/迭代一数据/players/info");
+			ArrayList<String> names = fl.getList();
+			for (String name : names) {
 				players.add(readFromPlayerFile(name));
 			}
 		} catch (IOException e) {
@@ -86,8 +88,7 @@ public class FileManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 return players;
-	 }
-	 
-	
+		return players;
+	}
+
 }
