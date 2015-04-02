5f5b009b46ab3cb271618c609de3fa15c2854608
NOT USING THIS <wn13nju@163.com>
2015-04-02 11:08:45 +0000

    修改team，添加team基本信息界面

--- CSEClient/CSEClient/src/main/java/newui/teamui/details/TeamDetailInfoPanel.java
+++ CSEClient/CSEClient/src/main/java/newui/teamui/details/TeamDetailInfoPanel.java
@@ -2,11 +2,27 @@ package newui.teamui.details;
 
 import javax.swing.JPanel;
 
+import vo.TeamVO;
+import businesslogic.Team;
+import businesslogicservice.TeamBLService;
+
 public class TeamDetailInfoPanel extends JPanel{
 
 	/**
 	 * 球队详情之基本信息
 	 */
 	private static final long serialVersionUID = 1L;
-
+	String chName,enName,abbrName;
+	TeamBLService team;
+	TeamVO vo;
+	public TeamDetailInfoPanel(String ch,String abbr){
+		//-----名字获取及VO生成----------
+		team=new Team();
+		chName=ch;
+		abbrName=abbr;
+		vo=team.getTeamBaseInfo(abbr);
+		enName=vo.getTeamName();
+		//----------------------------
+		
+	}
 }
