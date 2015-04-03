35d67fb79ff58105d745572821d2eb272f6e4194
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-04-03 13:44:25 +0000

    将获得比赛数据的方法移至match类中，详见Team构造方法前的说明

--- trunk/CSE/src/main/java/bl/Team.java
+++ trunk/CSE/src/main/java/bl/Team.java
@@ -20,10 +20,12 @@ public class Team implements TeamBLService {
 	private ArrayList<MatchVO> matches = new ArrayList<MatchVO>();
 	private ArrayList<TeamVO> teamAverageInfo = new ArrayList<TeamVO>();
 
+	// 删去了readFromMatchFile和getMatches两个私有方法，改为创建一个match类，从中得到比赛信息
 	public Team() {
 		// TODO 自动生成的构造函数存根
 		teamsBaseInfo = getTeams();
-		matches = getMatches();
+		Match match = new Match();
+		matches = match.getMatchData("全部", "全部", "全部", "全部");
 		teamAverageInfo = calculateTeamAverageInfo();
 	}
 
@@ -34,7 +36,7 @@ public class Team implements TeamBLService {
 		// ArrayList<MatchVO> result = new ArrayList<MatchVO>();
 		// result = team.getTeamSeasonInfo(season);
 		result = team.getTeamAverageInfo();
-		 result = team.getSeasonHotTeam(season, "比赛得分");
+		result = team.getSeasonHotTeam(season, "比赛得分");
 		// result=team.getTeamSeasonInfo(season);
 		// result = team.getRecentMatches("ATL");
 		// result=team.getMatches();
@@ -42,37 +44,38 @@ public class Team implements TeamBLService {
 
 		for (TeamVO vo : result) {
 			System.out.println(vo.getAbLocation() + " " + vo.getScore());
-//			System.out.println(vo.getAbLocation());
-//			System.out.println("winRate：" + vo.getWinRate());
-//			System.out.println("shootHitNum：" + vo.getShootHitNum());
-//			System.out.println("shootAttemptNum：" + vo.getShootAttemptNum());
-//			System.out.println("threeHitNum：" + vo.getThreeHitNum());
-//			System.out.println("threeAttemptNum：" + vo.getThreeAttemptNum());
-//			System.out.println("freeThrowHitNum：" + vo.getFreeThrowHitNum());
-//			System.out.println("freeThrowAttemptNum："
-//					+ vo.getFreeThrowAttemptNum());
-//			System.out.println("offenReboundNum：" + vo.getOffenReboundNum());
-//			System.out.println("defenReboundNum：" + vo.getDefenReboundNum());
-//			System.out.println("reboundNum：" + vo.getReboundNum());
-//			System.out.println("assistNum：" + vo.getAssistNum());
-//			System.out.println("stealNum：" + vo.getStealNum());
-//			System.out.println("blockNum：" + vo.getBlockNum());
-//			System.out.println("turnOverNum：" + vo.getTurnOverNum());
-//			System.out.println("foulNum：" + vo.getFoulNum());
-//			System.out.println("score：" + vo.getScore());
-//			System.out.println("shootHitRate:" + vo.getShootHitRate());
-//			System.out.println("threeHitRate:" + vo.getThreeHitRate());
-//			System.out.println("freeThrowHitRate:" + vo.getFreeThrowHitRate());
-//			System.out.println("offenRound:" + vo.getOffenRound());
-//			System.out.println("offenEfficiency:" + vo.getOffenEfficiency());
-//			System.out.println("defenEfficiency:" + vo.getDefenEfficiency());
-//			System.out.println("offenReboundEfficiency:"
-//					+ vo.getOffenReboundEfficiency());
-//			System.out.println("defenReboundEfficiency:"
-//					+ vo.getDefenReboundEfficiency());
-//			System.out.println("stealEfficiency:" + vo.getStealEfficiency());
-//			System.out.println("assistRate:" + vo.getAssistRate());
-//			System.out.println("----------------------------------");
+			// System.out.println(vo.getAbLocation());
+			// System.out.println("winRate：" + vo.getWinRate());
+			// System.out.println("shootHitNum：" + vo.getShootHitNum());
+			// System.out.println("shootAttemptNum：" + vo.getShootAttemptNum());
+			// System.out.println("threeHitNum：" + vo.getThreeHitNum());
+			// System.out.println("threeAttemptNum：" + vo.getThreeAttemptNum());
+			// System.out.println("freeThrowHitNum：" + vo.getFreeThrowHitNum());
+			// System.out.println("freeThrowAttemptNum："
+			// + vo.getFreeThrowAttemptNum());
+			// System.out.println("offenReboundNum：" + vo.getOffenReboundNum());
+			// System.out.println("defenReboundNum：" + vo.getDefenReboundNum());
+			// System.out.println("reboundNum：" + vo.getReboundNum());
+			// System.out.println("assistNum：" + vo.getAssistNum());
+			// System.out.println("stealNum：" + vo.getStealNum());
+			// System.out.println("blockNum：" + vo.getBlockNum());
+			// System.out.println("turnOverNum：" + vo.getTurnOverNum());
+			// System.out.println("foulNum：" + vo.getFoulNum());
+			// System.out.println("score：" + vo.getScore());
+			// System.out.println("shootHitRate:" + vo.getShootHitRate());
+			// System.out.println("threeHitRate:" + vo.getThreeHitRate());
+			// System.out.println("freeThrowHitRate:" +
+			// vo.getFreeThrowHitRate());
+			// System.out.println("offenRound:" + vo.getOffenRound());
+			// System.out.println("offenEfficiency:" + vo.getOffenEfficiency());
+			// System.out.println("defenEfficiency:" + vo.getDefenEfficiency());
+			// System.out.println("offenReboundEfficiency:"
+			// + vo.getOffenReboundEfficiency());
+			// System.out.println("defenReboundEfficiency:"
+			// + vo.getDefenReboundEfficiency());
+			// System.out.println("stealEfficiency:" + vo.getStealEfficiency());
+			// System.out.println("assistRate:" + vo.getAssistRate());
+			// System.out.println("----------------------------------");
 
 		}
 
@@ -146,148 +149,6 @@ public class Team implements TeamBLService {
 			teams.add(team);
 		}
 		return teams;
-	}
-
-	private MatchVO readFromMatchFile(String fileName) {
-		MatchVO matchVO;
-		String season;// 赛季
-		String date = null;// 比赛日期
-		String teams = null;// 对阵队伍
-		String score = null;// 比分
-
-		ArrayList<String> detailScores = new ArrayList<String>();// 各节比分
-		ArrayList<RecordVO> records = new ArrayList<RecordVO>();// 球员比分数据记录
-
-		String tp[] = fileName.split("matches");
-		season = tp[1].substring(1, 6);
-
-		try {
-			File file = new File(fileName);
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					new FileInputStream(file), "UTF-8"));
-			String temp = null;
-
-			temp = br.readLine();
-			String[] fisrtContent = temp.split(";");
-			date = fisrtContent[0];
-			teams = fisrtContent[1];
-			score = fisrtContent[2];
-
-			temp = br.readLine();
-			String[] scoresData = temp.split(";");
-			for (int i = 0; i < scoresData.length; i++) {
-				detailScores.add(scoresData[i]);
-			}
-
-			String team = null;// 球队
-			String playerName = null;// 球员名
-			String VOsition = null;// 位置
-			String presentTime = null;// 在场时间
-			int shootHitNum = 0;// 投篮命中数
-			int shootAttemptNum = 0;// 投篮出手数
-			int threeHitNum = 0;// 三分命中数
-			int threeAttemptNum = 0;// 三分出手数
-			int freeThrowHitNum = 0;// 罚球命中数
-			int freeThrowAttemptNum = 0;// 罚球出手数
-			int offenReboundNum = 0;// 进攻（前场）篮板数
-			int defenReboundNum = 0;// 防守（后场）篮板数
-			int reboundNum = 0;// 总篮板数
-			int assistNum = 0;// 助攻数
-			int stealNum = 0;// 抢断数
-			int blockNum = 0;// 盖帽数
-			int turnOverNum = 0;// 失误数
-			int foulNum = 0;// 犯规数
-			int personScore = 0;// 个人得分
-
-			boolean isComplete = false;
-			temp = br.readLine();
-			while (temp != null) {
-				if (fileName.contains(temp)) {
-					team = temp;
-					isComplete = false;
-				} else {
-					String[] line = temp.split(";");
-					playerName = line[0];
-					VOsition = line[1];
-					presentTime = DirtyDataManager.checkPresentTime(fileName,
-							team, playerName, line[2]);// 在场时间
-					shootHitNum = Integer.parseInt(line[3]);// 投篮命中数
-					shootAttemptNum = DirtyDataManager.checkShootAndHitNum(
-							fileName, Integer.parseInt(line[4]), shootHitNum);// 投篮出手数
-					threeHitNum = Integer.parseInt(line[5]);// 三分命中数
-					threeAttemptNum = DirtyDataManager.checkShootAndHitNum(
-							fileName, Integer.parseInt(line[6]), threeHitNum);// 三分出手数
-					freeThrowHitNum = Integer.parseInt(line[7]);// 罚球命中数
-					freeThrowAttemptNum = DirtyDataManager.checkShootAndHitNum(
-							fileName, Integer.parseInt(line[8]),
-							freeThrowHitNum);// 罚球出手数
-					offenReboundNum = Integer.parseInt(line[9]);// 进攻（前场）篮板数
-					defenReboundNum = Integer.parseInt(line[10]);// 防守（后场）篮板数
-					reboundNum = DirtyDataManager.checkReboundNum(fileName,
-							offenReboundNum, defenReboundNum,
-							Integer.parseInt(line[11]));// 总篮板数
-					assistNum = Integer.parseInt(line[12]);// 助攻数
-					stealNum = Integer.parseInt(line[13]);// 抢断数
-					blockNum = Integer.parseInt(line[14]);// 盖帽数
-					turnOverNum = Integer.parseInt(line[15]);// 失误数
-					foulNum = Integer.parseInt(line[16]);// 犯规数
-					personScore = DirtyDataManager.checkPersonScore(fileName,
-							line[17], temp);// 个人得分
-
-					isComplete = true;
-				}
-				if (isComplete) {
-					RecordVO recordVO = new RecordVO(team, playerName,
-							VOsition, presentTime, shootHitNum,
-							shootAttemptNum, threeHitNum, threeAttemptNum,
-							freeThrowHitNum, freeThrowAttemptNum,
-							offenReboundNum, defenReboundNum, reboundNum,
-							assistNum, stealNum, blockNum, turnOverNum,
-							foulNum, personScore);
-					records.add(recordVO);
-					isComplete = false;
-				}
-				temp = br.readLine();
-			}
-
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		String[] temp = teams.split("-");
-		String visitingTeam = temp[0];
-		String homeTeam = temp[1];
-
-		String[] s = score.split("-");
-		int visitingScore = Integer.parseInt(s[0]);
-		int homeScore = Integer.parseInt(s[1]);
-
-		matchVO = new MatchVO(season, date, visitingTeam, homeTeam,
-				visitingScore, homeScore, detailScores, records);
-		return matchVO;
-	}
-
-	private ArrayList<MatchVO> getMatches() {
-		// TODO 自动生成的方法存根
-		ArrayList<MatchVO> matches = new ArrayList<MatchVO>();
-		try {
-			FileList fl = new FileList("src/data/matches");
-			ArrayList<String> names = fl.getList();
-			for (String name : names) {
-				matches.add(readFromMatchFile(name));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return matches;
 	}
 
 	private ArrayList<TeamVO> calculateTeamAverageInfo() {
