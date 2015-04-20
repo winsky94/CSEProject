797119cc652d9d08cf4f08d0fc0bcdf67dfcfb7a
aslasp <aslasp@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-04-20 13:03:21 +0000

    增加最大化按钮

--- trunk/CSE/src/main/java/newui/TopPanel.java
+++ trunk/CSE/src/main/java/newui/TopPanel.java
@@ -15,92 +15,122 @@ import javax.swing.JPanel;
 
 import newui.mainui.MainFrame;
 
-public class TopPanel extends JPanel implements MouseListener{
+public class TopPanel extends JPanel implements MouseListener {
 
 	/**
 	 * 
 	 * 期望实现：页面跳转功能
 	 */
 	private static final long serialVersionUID = 1L;
-	MyButton miniBtn,exitBtn;
-	public TopPanel(){
+	MyButton miniBtn, maxBtn, exitBtn;
+	boolean isMax = false;
+
+	public TopPanel() {
 		GridBagLayout g = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
-		c.fill=GridBagConstraints.BOTH;
-		c.insets=new Insets(0,0, 0,0);
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(0, 0, 0, 0);
 		setLayout(g);
 		setBackground(Color.white);
-		//--------------------
-		JLabel blankLbl=new JLabel();
+		// --------------------
+		JLabel blankLbl = new JLabel();
 		c.gridx = 6;
 		c.gridwidth = 10;
-		c.weightx =100;
+		c.weightx = 100;
 		g.setConstraints(blankLbl, c);
 		add(blankLbl);
-		//---------------------
-		c.insets=new Insets(0,5, 0,5);
-		miniBtn=new MyButton(new ImageIcon("image/TopTitle/minimize.png"));
+		// ---------------------
+		c.insets = new Insets(0, 5, 0, 5);
+		//
+		miniBtn = new MyButton(new ImageIcon("image/TopTitle/minimize.png"));
 		c.gridx = 16;
 		c.gridwidth = 1;
-		c.weightx= 0.01;
+		c.weightx = 0.01;
 		miniBtn.addMouseListener(this);
 		g.setConstraints(miniBtn, c);
 		add(miniBtn);
-		//----------------------
-		exitBtn=new MyButton(new ImageIcon("image/TopTitle/exit.png"));
+		// ----------------------
+		maxBtn = new MyButton(new ImageIcon("image/TopTitle/max.png"));
 		c.gridx = 17;
-		c.gridwidth = 1;
-		c.weightx= 0.01;
+		maxBtn.addMouseListener(this);
+		g.setConstraints(maxBtn, c);
+		add(maxBtn);
+		// ----------------------
+		exitBtn = new MyButton(new ImageIcon("image/TopTitle/exit.png"));
+		c.gridx = 18;
 		exitBtn.addMouseListener(this);
 		g.setConstraints(exitBtn, c);
 		add(exitBtn);
 	}
+
 	public void mouseClicked(MouseEvent e) {
-		if(e.getSource()==exitBtn)
+		if (e.getSource() == exitBtn)
 			System.exit(0);
-		if(e.getSource()==miniBtn){
-			//监听
+		if (e.getSource() == maxBtn) {
+			// 监听
+			if (!isMax){
+				MainFrame.getInstance().setExtendedState(JFrame.MAXIMIZED_BOTH);
+				isMax=true;
+			}
+			else{
+				MainFrame.getInstance().setExtendedState(JFrame.NORMAL);
+				isMax=false;
+			}
+		}
+		if (e.getSource() == miniBtn) {
+			// 监听
 			MainFrame.getInstance().setExtendedState(JFrame.HIDE_ON_CLOSE);
 		}
-			
+
 	}
+
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	public void mouseEntered(MouseEvent e) {
-		if(e.getSource()==exitBtn){
+		if (e.getSource() == exitBtn) {
 			exitBtn.setIcon(new ImageIcon("image/TopTitle/exitFocus.png"));
 		}
-		if(e.getSource()==miniBtn){
+		if (e.getSource() == miniBtn) {
 			miniBtn.setIcon(new ImageIcon("image/TopTitle/minimizeFocus.png"));
 		}
-		
+		if (e.getSource() == maxBtn) {
+			maxBtn.setIcon(new ImageIcon("image/TopTitle/maxFocus.png"));
+		}
 	}
+
 	public void mouseExited(MouseEvent e) {
-		if(e.getSource()==exitBtn){
+		if (e.getSource() == exitBtn) {
 			exitBtn.setIcon(new ImageIcon("image/TopTitle/exit.png"));
 		}
-		if(e.getSource()==miniBtn){
+		if (e.getSource() == maxBtn) {
+			maxBtn.setIcon(new ImageIcon("image/TopTitle/max.png"));
+		}
+		if (e.getSource() == miniBtn) {
 			miniBtn.setIcon(new ImageIcon("image/TopTitle/minimize.png"));
 		}
-		
+
 	}
-	
+
 }
-class MyButton extends JLabel{
+
+class MyButton extends JLabel {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public MyButton(ImageIcon img){
+
+	public MyButton(ImageIcon img) {
 		super(img);
 		this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	}
-	
+
 }
