a040fc768e76a65a4b02e138caf350f6988f0511
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-04-21 14:56:21 +0000

    将表格内容减少，拆分成两个表格

--- CSE/src/main/java/newui/tables/TeamTableModel.java
+++ CSE/src/main/java/newui/tables/TeamTableModel.java
@@ -14,20 +14,26 @@ public class TeamTableModel extends MyTableModel {
 	private static final long serialVersionUID = 1L;
 	TeamBLService team = new Team();
 	ArrayList<ArrayList<Object>> content = new ArrayList<ArrayList<Object>>();
-	static String[] head = { "球队名称", "简称", "比赛场数", "投篮命中数", "投篮出手次数", "投篮命中率",
-			"三分命中数", "三分出手数", "三分命中率", "罚球命中数", "罚球出手数", "罚球命中率", "进攻篮板数",
-			"防守篮板数", "篮板数", "进攻篮板效率", "防守篮板效率", "进攻回合", "进攻效率", "防守效率", "助攻数",
-			"助攻率", "抢断数", "抢断效率", "盖帽数", "失误数", "犯规数", "比赛得分", "胜率" };
+	public int headmodel;
+	static String[] head;
+	// static String[] head = { "球队名称", "简称", "比赛场数", "投篮命中数", "投篮出手次数",
+	// "投篮命中率",
+	// "三分命中数", "三分出手数", "三分命中率", "罚球命中数", "罚球出手数", "罚球命中率", "进攻篮板数",
+	// "防守篮板数", "篮板数", "进攻篮板效率", "防守篮板效率", "进攻回合", "进攻效率", "防守效率", "助攻数",
+	// "助攻率", "抢断数", "抢断效率", "盖帽数", "失误数", "犯规数", "比赛得分", "胜率" };
 
-	String[] example = { "金大大", "10", "10", "10", "100%", "10", "10", "100%",
-			"0", "0", "100%", "1", "2", "3", "100%", "2", "100%", "100%", "1",
-			"100%", "100%", "1", "100%", "1", "0", "0", "100", "100%" };
+	String[] headbase = { "球队名称", "简称", "比赛场数", "投篮命中率", "三分命中率", "罚球命中率",
+			"进攻篮板数", "防守篮板数", "篮板数", "助攻数", "抢断数", "盖帽数", "失误数", "犯规数", "比赛得分" };
+	String[] headhigh = { "球队名称", "简称", "进攻篮板效率", "防守篮板效率", "进攻回合", "进攻效率",
+			"防守效率", "助攻率", "抢断效率", "胜率" };
 
-	public TeamTableModel() {
-		// ArrayList<Object> e = new ArrayList<Object>();
-		// for (Object str : example)
-		// e.add(str);
-		// content.add(e);
+	public TeamTableModel(int model) {
+		if (model == 0) {
+			head = headbase;
+		} else {
+			head = headhigh;
+		}
+		headmodel = model;
 	}
 
 	@Override
@@ -58,6 +64,16 @@ public class TeamTableModel extends MyTableModel {
 		return head;
 	}
 
+	// 切换表头
+	public void setHead(int model) {
+		// model=0基础 model=1高阶
+		if (model == 0)
+			head = headbase;
+		else
+			head = headhigh;
+		headmodel = model;
+	}
+
 	public void Refresh(String model) {
 		ArrayList<TeamVO> teamvo;
 		if (model.equals("赛季"))
@@ -67,7 +83,12 @@ public class TeamTableModel extends MyTableModel {
 
 		}
 		if (teamvo != null && teamvo.size() != 0) {
-			refreshContent(teamvo);
+			// refreshContent(teamvo);
+			if (headmodel == 0)
+				refreshBase(teamvo);
+			else{
+				refreshHigh(teamvo);
+			}
 		}
 		/*
 		 * {"球队名称","比赛场数","投篮命中数","投篮出手次数","投篮命中率","三分命中数","三分出手数",
@@ -79,39 +100,84 @@ public class TeamTableModel extends MyTableModel {
 
 	}
 
-	public void refreshContent(ArrayList<TeamVO> result) {
-		System.out.println("TeamTableModel.refreshContent()");
+	// public void refreshContent(ArrayList<TeamVO> result) {
+	// content.clear();
+	// for (TeamVO vo : result) {
+	// ArrayList<Object> line = new ArrayList<Object>();
+	// line.add(vo.getTeamName());
+	// line.add(vo.getAbLocation());
+	// line.add(vo.getMatchesNum());
+	// line.add(vo.getShootHitNum());
+	// line.add(vo.getShootAttemptNum());
+	// line.add(vo.getShootHitRate());
+	// line.add(vo.getThreeHitNum());
+	// line.add(vo.getThreeAttemptNum());
+	// line.add(vo.getThreeHitRate());
+	// line.add(vo.getFreeThrowHitNum());
+	// line.add(vo.getFreeThrowAttemptNum());
+	// line.add(vo.getFreeThrowHitRate());
+	// line.add(vo.getOffenReboundNum());
+	// line.add(vo.getDefenReboundNum());
+	// line.add(vo.getReboundNum());
+	// line.add(vo.getOffenReboundEfficiency());
+	// line.add(vo.getDefenReboundEfficiency());
+	// line.add(vo.getOffenRound());
+	// line.add(vo.getOffenEfficiency());
+	// line.add(vo.getDefenEfficiency());
+	// line.add(vo.getAssistNum());
+	// line.add(vo.getAssistRate());
+	// line.add(vo.getStealNum());
+	// line.add(vo.getStealEfficiency());
+	// line.add(vo.getBlockNum());
+	// line.add(vo.getTurnOverNum());
+	// line.add(vo.getFoulNum());
+	// line.add(vo.getScore());
+	// line.add(vo.getWinRate());
+	// content.add(line);
+	// }
+	// }
+
+	// "球队名称", "简称", "比赛场数", "投篮命中率", "三分命中率", "罚球命中率",
+	// "进攻篮板数", "防守篮板数", "篮板数", "助攻数", "抢断数", "盖帽数", "失误数", "犯规数", "比赛得分"
+	public void refreshBase(ArrayList<TeamVO> result) {
 		content.clear();
 		for (TeamVO vo : result) {
 			ArrayList<Object> line = new ArrayList<Object>();
 			line.add(vo.getTeamName());
 			line.add(vo.getAbLocation());
 			line.add(vo.getMatchesNum());
-			line.add(vo.getShootHitNum());
-			line.add(vo.getShootAttemptNum());
 			line.add(vo.getShootHitRate());
-			line.add(vo.getThreeHitNum());
-			line.add(vo.getThreeAttemptNum());
 			line.add(vo.getThreeHitRate());
-			line.add(vo.getFreeThrowHitNum());
-			line.add(vo.getFreeThrowAttemptNum());
 			line.add(vo.getFreeThrowHitRate());
 			line.add(vo.getOffenReboundNum());
 			line.add(vo.getDefenReboundNum());
 			line.add(vo.getReboundNum());
+			line.add(vo.getAssistNum());
+			line.add(vo.getStealNum());
+			line.add(vo.getBlockNum());
+			line.add(vo.getTurnOverNum());
+			line.add(vo.getFoulNum());
+			line.add(vo.getScore());
+			content.add(line);
+		}
+	}
+
+	// "球队名称", "简称", "进攻篮板效率", "防守篮板效率", "进攻回合", "进攻效率",
+	// "防守效率", "助攻率", "抢断效率", "胜率"
+	public void refreshHigh(ArrayList<TeamVO> result) {
+		content.clear();
+		for (TeamVO vo : result) {
+			ArrayList<Object> line = new ArrayList<Object>();
+			line.add(vo.getTeamName());
+			line.add(vo.getAbLocation());
+			line.add(vo.getMatchesNum());
 			line.add(vo.getOffenReboundEfficiency());
 			line.add(vo.getDefenReboundEfficiency());
 			line.add(vo.getOffenRound());
 			line.add(vo.getOffenEfficiency());
 			line.add(vo.getDefenEfficiency());
-			line.add(vo.getAssistNum());
 			line.add(vo.getAssistRate());
-			line.add(vo.getStealNum());
 			line.add(vo.getStealEfficiency());
-			line.add(vo.getBlockNum());
-			line.add(vo.getTurnOverNum());
-			line.add(vo.getFoulNum());
-			line.add(vo.getScore());
 			line.add(vo.getWinRate());
 			content.add(line);
 		}
@@ -120,8 +186,10 @@ public class TeamTableModel extends MyTableModel {
 	public void SearchRefresh(Object vv) {
 		ArrayList<TeamVO> v = (ArrayList<TeamVO>) vv;
 		if (v != null && v.size() != 0) {
-
-			refreshContent(v);
+			if (headmodel == 0)
+				refreshBase(v);
+			else
+				refreshHigh(v);
 		}
 
 	}
