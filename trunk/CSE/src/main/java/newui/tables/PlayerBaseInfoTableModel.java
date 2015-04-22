f61f357f132248cbc90722373845fb85ef25a30a
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-04-22 16:16:19 +0000

    修复显示搜索结果有几条的label的空指针异常

--- trunk/CSE/src/main/java/newui/tables/PlayerBaseInfoTableModel.java
+++ trunk/CSE/src/main/java/newui/tables/PlayerBaseInfoTableModel.java
@@ -1,20 +1,15 @@
 package newui.tables;
 
-import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 
 import newui.Service;
 import newui.playerui.BaseInfoPlayerThread;
-import bl.player.Player;
-import bl.team.Team;
-import blservice.PlayerBLService;
 import vo.PlayerVO;
-
+import blservice.PlayerBLService;
 
 public class PlayerBaseInfoTableModel extends MyTableModel {
 
@@ -24,16 +19,17 @@ public class PlayerBaseInfoTableModel extends MyTableModel {
 
 	ArrayList<ArrayList<Object>> content = new ArrayList<ArrayList<Object>>();
 	private PlayerBLService player;
-	//private ArrayList<ImageIcon> imgList = new ArrayList<ImageIcon>();
-	//private ArrayList<PlayerVO> playerlist;
+	// private ArrayList<ImageIcon> imgList = new ArrayList<ImageIcon>();
+	// private ArrayList<PlayerVO> playerlist;
 	private JTable currentTable;
 	private JLabel label;
 	private ArrayList<String> nameList;
+
 	public PlayerBaseInfoTableModel() {
 
 		player = Service.player;
-		//playerlist = new ArrayList<PlayerVO>();
-		
+		// playerlist = new ArrayList<PlayerVO>();
+
 	}
 
 	public void setCurrentTable(JTable t) {
@@ -59,9 +55,10 @@ public class PlayerBaseInfoTableModel extends MyTableModel {
 		return content.get(rowIndex).get(columnIndex);
 	}
 
-	public void setValueAt(Object value,int row,int column){
+	public void setValueAt(Object value, int row, int column) {
 		content.get(row).set(column, value);
 	}
+
 	public String getColumnName(int col) {
 		return head[col];
 	}
@@ -71,20 +68,20 @@ public class PlayerBaseInfoTableModel extends MyTableModel {
 	}
 
 	public void sortByCharacter(String character) {
-		char c=character.charAt(0);
-		ArrayList<PlayerVO> v=player.getPlayersByInitialName(c);
-		if(v.size()==0||v==null){
+		char c = character.charAt(0);
+		ArrayList<PlayerVO> v = player.getPlayersByInitialName(c);
+		if (v.size() == 0 || v == null) {
 			content.clear();
-		}else{
+		} else {
 			Refresh(v);
 		}
 	}
 
 	public void findByTeam(String tName) {
-		ArrayList<PlayerVO> v=player.getPlayersByTeam(tName);
-		if(v.size()==0||v==null){
+		ArrayList<PlayerVO> v = player.getPlayersByTeam(tName);
+		if (v.size() == 0 || v == null) {
 			content.clear();
-		}else{
+		} else {
 			Refresh(v);
 		}
 	}
@@ -99,17 +96,16 @@ public class PlayerBaseInfoTableModel extends MyTableModel {
 
 	}
 
-	
 	public void Refresh(ArrayList<PlayerVO> list) {
-	
+
 		content.clear();
-	
-		nameList=new ArrayList<String>();
-	
+
+		nameList = new ArrayList<String>();
+
 		for (PlayerVO vo : list) {
 			ArrayList<Object> line = new ArrayList<Object>();
 			String name = vo.getName();
-			
+
 			nameList.add(name);
 			line.add("加载中");
 			line.add(name);
@@ -122,69 +118,49 @@ public class PlayerBaseInfoTableModel extends MyTableModel {
 			line.add(vo.getExp());
 			content.add(line);
 
-			
-
 		}
-		
-		//线程加载图片
-		BaseInfoPlayerThread thre=new BaseInfoPlayerThread(nameList,currentTable);
+
+		// 线程加载图片
+		BaseInfoPlayerThread thre = new BaseInfoPlayerThread(nameList,
+				currentTable);
 		thre.start();
 		currentTable.revalidate();
-		
-
-	}
-/*
-	public void SortRefresh(ArrayList<PlayerVO> list, ArrayList<ImageIcon> img) {
-		content.clear();
-		int i = 0;
-		for (PlayerVO vo : list) {
-			ArrayList<Object> line = new ArrayList<Object>();
-			ImageIcon icon = new ImageIcon(img
-					.get(i)
-					.getImage()
-					.getScaledInstance(
-							currentTable.getColumn(
-									currentTable.getColumnName(0)).getWidth(),
-							40
-						,
-							Image.SCALE_DEFAULT));
-			line.add(icon);
-			//line.add("头像");
-			line.add(vo.getName());
-			line.add(vo.getOwingTeam());
-			line.add(vo.getPosition());
-			line.add(vo.getHeight());
-			line.add(vo.getWeight());
-			line.add(vo.getBirth());
-			line.add(vo.getAge());
-			line.add(vo.getExp());
-			content.add(line);
-			currentTable.revalidate();
-			i++;
-
-		}
 
 	}
 
-	/*public ArrayList<ImageIcon> getImgList() {
-		return imgList;
-	}*/
-	
-	public void SearchRefresh(Object vv){
-		ArrayList<PlayerVO> v=(ArrayList<PlayerVO>)vv;
-		if(v!=null&&v.size()!=0){
-			
+	/*
+	 * public void SortRefresh(ArrayList<PlayerVO> list, ArrayList<ImageIcon>
+	 * img) { content.clear(); int i = 0; for (PlayerVO vo : list) {
+	 * ArrayList<Object> line = new ArrayList<Object>(); ImageIcon icon = new
+	 * ImageIcon(img .get(i) .getImage() .getScaledInstance(
+	 * currentTable.getColumn( currentTable.getColumnName(0)).getWidth(), 40 ,
+	 * Image.SCALE_DEFAULT)); line.add(icon); //line.add("头像");
+	 * line.add(vo.getName()); line.add(vo.getOwingTeam());
+	 * line.add(vo.getPosition()); line.add(vo.getHeight());
+	 * line.add(vo.getWeight()); line.add(vo.getBirth()); line.add(vo.getAge());
+	 * line.add(vo.getExp()); content.add(line); currentTable.revalidate(); i++;
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * /*public ArrayList<ImageIcon> getImgList() { return imgList; }
+	 */
+
+	public void SearchRefresh(Object vv) {
+		ArrayList<PlayerVO> v = (ArrayList<PlayerVO>) vv;
+		if (v != null && v.size() != 0) {
 			Refresh(v);
-		}else
+		} else
 			content.clear();
-		label.setText("在球员中检索到"+v.size()+"条符合关键字"+ss+"的结果...");
+		if (label != null) {
+			label.setText("在球员中检索到" + v.size() + "条符合关键字" + ss + "的结果...");
+		}
 	}
-	
-	
-	public void setCurrentLable(JLabel label){
-		this.label=label;
-		
+
+	public void setCurrentLable(JLabel label) {
+		this.label = label;
+
 	}
-	
-	
+
 }
