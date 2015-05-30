05c40083f1b02a1197031aacc53b3b78ddc241ca
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-05-30 04:12:36 +0000

    实现方法

--- trunk/CSEIII/src/main/java/bl/Match.java
+++ trunk/CSEIII/src/main/java/bl/Match.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 import vo.MatchVO;
 import blService.MatchBLService;
+import data.MatchData;
+import dataservice.MatchDataService;
 
 public class Match implements MatchBLService {
+	MatchDataService matchData;
 
 	public static void main(String[] args) {
 		MatchBLService match = new Match();
@@ -14,22 +17,30 @@ public class Match implements MatchBLService {
 		System.out.println(result.get(0));
 	}
 
+	public Match() {
+		matchData = new MatchData();
+	}
+
 	public ArrayList<String> getAllSeasons() {
 		// TODO 自动生成的方法存根
 		ArrayList<String> result = new ArrayList<String>();
-
+		result=matchData.getAllSeasons();
 		return result;
 	}
 
 	public ArrayList<MatchVO> getMatchData(String season, String date,
 			String homeTeam, String visitingTeam) {
 		// TODO 自动生成的方法存根
-		return null;
+		ArrayList<MatchVO> result=new ArrayList<MatchVO>();
+		result=matchData.getMatchData(season, date, homeTeam, visitingTeam);
+		return result;
 	}
 
-	public ArrayList<MatchVO> getMatches(String name) {
+	public ArrayList<MatchVO> getMatchesByTeam(String name) {
 		// TODO 自动生成的方法存根
-		return null;
+		ArrayList<MatchVO> result=new ArrayList<MatchVO>();
+		result=matchData.getMatchesByTeam(name);
+		return result;
 	}
 
 }
