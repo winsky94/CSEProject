a463c3c1fcc8d78e45ccb561f52bfa8d4e5efbf9
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-05-30 11:37:33 +0000

    增加赛季和比赛数据的比赛类型接口

--- trunk/CSEIII/src/main/java/blService/TeamBLService.java
+++ trunk/CSEIII/src/main/java/blService/TeamBLService.java
@@ -20,18 +20,22 @@ public interface TeamBLService {
 	 * 
 	 * @param season
 	 *            赛季
+	 * @param type
+	 *            比赛类型
 	 * @return 球队赛季统计数据，vo的所有初始值均进行了初始化
 	 * 
 	 */
-	public ArrayList<TeamVO> getTeamSeasonInfo(String season);
+	public ArrayList<TeamVO> getTeamSeasonInfo(String season, String type);
 
 	/**
 	 * 得到球队的该场均的技术统计数据
 	 * 
+	 * @param type
+	 *            比赛类型
 	 * @return 球队场均统计数据，VO的所有初始值均进行了初始化
 	 * 
 	 */
-	public ArrayList<TeamVO> getTeamAverageInfo();
+	public ArrayList<TeamVO> getTeamAverageInfo(String type);
 
 	/**
 	 * 模糊查询球队的基础信息，球队名可以是名称，也可以是缩写，大小写均可
@@ -47,20 +51,25 @@ public interface TeamBLService {
 	 * 
 	 * @param season
 	 *            赛季
+	 * @param type
+	 *            比赛类型
 	 * @param name
 	 *            球队名称
 	 * @return 符合模糊查询条件的球队对象的列表
 	 */
-	public ArrayList<TeamVO> getTeamSeasonInfo(String season, String name);
+	public ArrayList<TeamVO> getTeamSeasonInfo(String season, String type,
+			String name);
 
 	/**
 	 * 模糊查询球队的场均技术信息，球队名可以是名称，也可以是缩写，大小写均可
 	 * 
 	 * @param name
 	 *            球队名称
+	 * @param type
+	 *            比赛类型
 	 * @return 符合模糊查询条件的球队对象的列表
 	 */
-	public ArrayList<TeamVO> getTeamAverageInfo(String name);
+	public ArrayList<TeamVO> getTeamAverageInfo(String type, String name);
 
 	/**
 	 * 根据某一项技术分析项，将球队按某个赛季的该项数据进行升降序排序
@@ -75,7 +84,7 @@ public interface TeamBLService {
 	 * @return 按照所给条件排好序的球队列表
 	 */
 	public ArrayList<TeamVO> getOrderedTeamsBySeason(String season,
-			String condition, String order,int num);
+			String type, String condition, String order, int num);
 
 	/**
 	 * 根据某一项技术分析项，将球队按场均该项数据进行升降序排序
@@ -89,8 +98,8 @@ public interface TeamBLService {
 	 * 
 	 * @return 按照所给条件排好序的球队列表
 	 */
-	public ArrayList<TeamVO> getOrderedTeamsByAverage(String condition,
-			String order,int num);
+	public ArrayList<TeamVO> getOrderedTeamsByAverage(String type,String condition,
+			String order, int num);
 
 	/**
 	 * 根据球队缩写得到球队队徽
@@ -126,6 +135,7 @@ public interface TeamBLService {
 	 *            筛选条件
 	 * @return 返回到 目前为止所有参加过比赛的球队中筛选出前 5 名球队（按照 降序排列进行筛选）
 	 */
-	public ArrayList<TeamVO> getSeasonHotTeam(String season, String column,int num);
-	
+	public ArrayList<TeamVO> getSeasonHotTeam(String season, String column,
+			int num);
+
 }
