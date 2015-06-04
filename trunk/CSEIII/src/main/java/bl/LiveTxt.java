7c0241a0ee69012a0dc51bf29ace32112d6401c2
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-06-04 11:54:01 +0000

    增加main方法测试运行时间

--- trunk/CSEIII/src/main/java/bl/LiveTxt.java
+++ trunk/CSEIII/src/main/java/bl/LiveTxt.java
@@ -13,6 +13,26 @@ public class LiveTxt implements LiveTxtBLService {
 		liveTxtData = new LiveTxtData();
 	}
 
+	public static void main(String[] args){
+		long start = System.currentTimeMillis();
+		LiveTxt liveTxt = new LiveTxt();
+		System.out.println(liveTxt.getLiveTxt("14-15", "04-04", "DET-CHI").size());
+		long end1 = System.currentTimeMillis();
+		System.out.println("运行时间：" + (end1 - start) + "毫秒");// 应该是end - start
+//		System.out.println(liveTxt.getPlayerName(76002));
+//		long end2 = System.currentTimeMillis();
+//		System.out.println("运行时间：" + (end2 - end1) + "毫秒");// 应该是end - start
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.println(liveTxt.getPlayerName(51));
+//		long end3 = System.currentTimeMillis();
+//		System.out.println("运行时间：" + (end3 - end2-3000) + "毫秒");// 应该是end - start
+	}
+	
 	public ArrayList<LiveMatchDetailVO> getLiveTxt(String season, String date,
 			String teams) {
 		// TODO Auto-generated method stub
