13f6500f500c2bcc490a2ae01578a1cfcb5e3852
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-06-04 04:06:35 +0000

    增加接口注释

--- CSEIII/src/main/java/blService/LiveTxtBLService.java
+++ CSEIII/src/main/java/blService/LiveTxtBLService.java
@@ -5,5 +5,17 @@ import java.util.ArrayList;
 import vo.LiveMatchDetailVO;
 
 public interface LiveTxtBLService {
-     public ArrayList<LiveMatchDetailVO> getLiveTxt(String season,String date,String teams);
+	/**
+	 * 获得一场比赛的直播信息
+	 * 
+	 * @param season
+	 *            赛季
+	 * @param date
+	 *            日期
+	 * @param teams
+	 *            对阵队伍 客队-主队
+	 * @return 当前比赛的直播信息
+	 */
+	public ArrayList<LiveMatchDetailVO> getLiveTxt(String season, String date,
+			String teams);
 }
