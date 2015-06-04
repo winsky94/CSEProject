8554122aad2b3326ea34d11d99c9012b9fd38144
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-06-04 04:06:56 +0000

    增加直播的接口

---
+++ trunk/CSEIII/src/main/java/dataservice/LiveTxtDataService.java
@@ -0,0 +1,21 @@
+package dataservice;
+
+import java.util.ArrayList;
+
+import vo.LiveMatchDetailVO;
+
+public interface LiveTxtDataService {
+	/**
+	 * 获得一场比赛的直播信息
+	 * 
+	 * @param season
+	 *            赛季
+	 * @param date
+	 *            日期
+	 * @param teams
+	 *            对阵队伍 客队-主队
+	 * @return 当前比赛的直播信息
+	 */
+	public ArrayList<LiveMatchDetailVO> getLiveTxt(String season, String date,
+			String teams);
+}
