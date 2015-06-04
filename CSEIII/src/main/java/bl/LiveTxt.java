7af37bf1804f792c7246859bd7dbb09d6ea11212
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-06-04 04:06:27 +0000

    获得data层直播数据

--- CSEIII/src/main/java/bl/LiveTxt.java
+++ CSEIII/src/main/java/bl/LiveTxt.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 
 import vo.LiveMatchDetailVO;
 import blService.LiveTxtBLService;
+import data.LiveTxtData;
 
-public class LiveTxt implements LiveTxtBLService{
+public class LiveTxt implements LiveTxtBLService {
+	private LiveTxtData liveTxtData;
+
+	public LiveTxt() {
+		liveTxtData = new LiveTxtData();
+	}
 
 	public ArrayList<LiveMatchDetailVO> getLiveTxt(String season, String date,
 			String teams) {
 		// TODO Auto-generated method stub
-		return null;
+		return liveTxtData.getLiveTxt(season, date, teams);
 	}
-  
+
 }
