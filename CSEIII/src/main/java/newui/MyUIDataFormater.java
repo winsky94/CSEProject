e883c77ce403b553daac73a1606ebcc1a203f6cf
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-06-06 15:03:36 +0000

    新增规范小数位数的类

---
+++ CSEIII/src/main/java/newui/MyUIDataFormater.java
@@ -0,0 +1,21 @@
+package newui;
+
+import java.text.DecimalFormat;
+
+public class MyUIDataFormater {
+
+	public static void main(String[] args) {
+		System.out.println(formatTo3(1.2));
+	}
+	
+	/**
+	 * 将传入的double型小数转为特定的位数并作为一个string返回
+	 * @param originalNum 原始的double型小数
+	 * @return
+	 */
+	public static String formatTo3(double originalNum) {
+		DecimalFormat dec = new DecimalFormat("0.000");
+		return dec.format(originalNum);
+	}
+	
+}
