1c41c24b8891aed69143cc6f58b9c65aaaeaa5d1
winsky <winsky@b520c39a-0dbc-4437-8564-d66a5b0b1dbe>
2015-06-11 15:34:12 +0000

    增加team获得场均数据方法的赛季参数

--- trunk/CSEIII/src/main/java/blService/TeamBLService.java
+++ trunk/CSEIII/src/main/java/blService/TeamBLService.java
@@ -2,8 +2,6 @@ package blService;
 
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
-
 import vo.MatchVO;
 import vo.TeamVO;
 
@@ -30,12 +28,14 @@ public interface TeamBLService {
 	/**
 	 * 得到球队的该场均的技术统计数据
 	 * 
+	 * @param season
+	 *            赛季
 	 * @param type
 	 *            比赛类型
 	 * @return 球队场均统计数据，VO的所有初始值均进行了初始化
 	 * 
 	 */
-	public ArrayList<TeamVO> getTeamAverageInfo(String type);
+	public ArrayList<TeamVO> getTeamAverageInfo(String season, String type);
 
 	/**
 	 * 模糊查询球队的基础信息，球队名可以是名称，也可以是缩写，大小写均可
@@ -63,13 +63,17 @@ public interface TeamBLService {
 	/**
 	 * 模糊查询球队的场均技术信息，球队名可以是名称，也可以是缩写，大小写均可
 	 * 
-	 * @param name
-	 *            球队名称
+	 * @param season
+	 *            赛季
 	 * @param type
 	 *            比赛类型
+	 * @param name
+	 *            球队名称
+	 * 
 	 * @return 符合模糊查询条件的球队对象的列表
 	 */
-	public ArrayList<TeamVO> getTeamAverageInfo(String type, String name);
+	public ArrayList<TeamVO> getTeamAverageInfo(String season, String type,
+			String name);
 
 	/**
 	 * 根据某一项技术分析项，将球队按某个赛季的该项数据进行升降序排序
@@ -91,6 +95,8 @@ public interface TeamBLService {
 	 * 
 	 * @param season
 	 *            赛季
+	 * @param type
+	 *            比赛类型
 	 * @param condition
 	 *            排序条件，即某一项技术分析项，如：篮板率(英文)
 	 * @param order
@@ -98,9 +104,9 @@ public interface TeamBLService {
 	 * 
 	 * @return 按照所给条件排好序的球队列表
 	 */
-	public ArrayList<TeamVO> getOrderedTeamsByAverage(String type,
-			String condition, String order, int num);
-	
+	public ArrayList<TeamVO> getOrderedTeamsByAverage(String season,
+			String type, String condition, String order, int num);
+
 	/**
 	 * 根据球队名称查找该球队近期比赛
 	 * 
