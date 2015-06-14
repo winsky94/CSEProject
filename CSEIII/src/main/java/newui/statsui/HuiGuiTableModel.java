ea2f9fcd27b0fa0f83c1b515c8c51de9a93d587e
winsky94 <13187623+winsky94@users.noreply.github.com>
2015-06-14 16:42:12 +0000

    增加表格的model的行和列

--- CSEIII/src/main/java/newui/statsui/HuiGuiTableModel.java
+++ CSEIII/src/main/java/newui/statsui/HuiGuiTableModel.java
@@ -12,8 +12,8 @@ public class HuiGuiTableModel extends AbstractTableModel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	String[][] content=new String[2][7];
-	String[] title={"因素","效率值","GmSc效率值","得分-篮板-助攻","得分","篮板","助攻"};
+	String[][] content=new String[3][10];
+	String[] title={"因素","出场次数","得分","篮板","助攻","抢断","失误","效率值","GmSc效率值","得分-篮板-助攻"};
 	public HuiGuiTableModel() {
 		PlayerStatistic ps=new PlayerStatistic();
 		HashMap<String, ArrayList<String>> map=ps.start();
@@ -23,29 +23,56 @@ public class HuiGuiTableModel extends AbstractTableModel{
 		ArrayList<String> sclist=map.get("score");
 		ArrayList<String> relist=map.get("rebound");
 		ArrayList<String> aslist=map.get("assist");
+		ArrayList<String> malist=map.get("matchNum");
+		ArrayList<String> stlist=map.get("steal");
+		ArrayList<String> folist=map.get("foul");
 		content[0][0]="F值";
 		content[1][0]="拟合度";
-		content[0][1]=efflist.get(0);
-		content[1][1]=efflist.get(1);
-		content[0][2]=gmsclist.get(0);
-		content[1][2]=gmsclist.get(1);
-		content[0][3]=scorelist.get(0);
-		content[1][3]=scorelist.get(1);
-		content[0][4]=sclist.get(0);
-		content[1][4]=sclist.get(1);
-		content[0][5]=relist.get(0);
-		content[1][5]=relist.get(1);
-		content[0][6]=aslist.get(0);
-		content[1][6]=aslist.get(1);
+		content[2][0]="回归系数";
+		content[0][1]=malist.get(0);
+		content[1][1]=malist.get(1);
+		content[2][1]=malist.get(2);
+		
+		content[0][2]=sclist.get(0);
+		content[1][2]=sclist.get(1);
+		content[2][2]=sclist.get(2);
+		
+		content[0][3]=relist.get(0);
+		content[1][3]=relist.get(1);
+		content[2][3]=relist.get(2);
+		
+		content[0][4]=aslist.get(0);
+		content[1][4]=aslist.get(1);
+		content[2][4]=aslist.get(2);
+		
+		content[0][5]=stlist.get(0);
+		content[1][5]=stlist.get(1);
+		content[2][5]=stlist.get(2);
+		
+		content[0][6]=folist.get(0);
+		content[1][6]=folist.get(1);
+		content[2][6]=folist.get(2);
+		
+		content[0][7]=efflist.get(0);
+		content[1][7]=efflist.get(1);
+		content[2][7]=efflist.get(2);
+		
+		content[0][8]=gmsclist.get(0);
+		content[1][8]=gmsclist.get(1);
+		content[2][8]=gmsclist.get(2);
+		
+		content[0][9]=scorelist.get(0);
+		content[1][9]=scorelist.get(1);
+		content[2][9]=scorelist.get(2);
 	}
 	@Override
 	public int getRowCount() {
-		return 2;
+		return 3;
 	}
 
 	@Override
 	public int getColumnCount() {
-		return 7;
+		return 10;
 	}
 	public String getColumnName(int c){
 		return title[c];
